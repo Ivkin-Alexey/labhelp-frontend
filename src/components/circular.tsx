f695c0b065e37733bab70f14f625d9c232f9327b
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2024-12-20 01:19:20 +0300

    style: centering the slider:

---
+++ src/components/circular.tsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from "@mui/material";
+
+export default function Circular() {
+    return (
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+        >
+          <CircularProgress size="60px" />
+        </Box>
+      )
+}
