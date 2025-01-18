8e222eb9aeb7f6770f0515341be64caeddfc004b
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2025-01-19 01:52:38 +0300

    feat: implement image carousel

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,7 +1,8 @@
-import { createContext, useMemo} from 'react'
+import { createContext, useEffect, useMemo, useState } from 'react'
 
 import { Outlet } from 'react-router-dom'
 
+import Circular from './circular'
 import Header from './header/header'
 import useTheme from '../app/hooks/useTheme'
 
@@ -12,10 +13,29 @@ export default function Root() {
 
   const memoized = useMemo(() => ({ color, toggle }), [color, toggle])
 
+  const [isLoading, setIsLoading] = useState(true)
+
+  // useEffect(() => {
+  //   window.onload = () => {
+  //     setIsLoading(false)
+  //   }
+
+  //   return () => {
+  //     window.onload = null
+  //   }
+  // }, [])
+
+  // function renderContent() {
+  //   if (isLoading) {
+  //     return <Circular />
+  //   }
+
+  //   return 
+  // }
+
   return (
     <ThemeContext.Provider value={memoized}>
-      <Header />
-      <Outlet />
+      {/* <Header /> */ <Outlet />}
     </ThemeContext.Provider>
   )
 }
