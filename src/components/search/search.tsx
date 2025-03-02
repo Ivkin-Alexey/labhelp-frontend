8d6a02668dde5c807d709c7aad32a8b9ffaa1ff7
Ivkin-Alexey <ivkin.alexey.sergeevich@gmail.com>
2025-03-03 00:41:40 +0300

    feat: complete task

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,8 +1,10 @@
 import type { SyntheticEvent } from 'react'
 import { useEffect, useLayoutEffect, useMemo, useState, useRef } from 'react'
 
-import { AutocompleteInputChangeReason, SxProps, Theme, useMediaQuery } from '@mui/material'
+import type { AutocompleteInputChangeReason, Theme } from '@mui/material'
+import { SxProps, useMediaQuery } from '@mui/material'
 import { Button, Stack, Typography } from '@mui/material'
+import zIndex from '@mui/material/styles/zIndex'
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom'
 
 import EquipmentFilters from './equipment-filters'
@@ -10,9 +12,15 @@ import SearchInput from './search-input'
 import { PAGE, PAGE_SIZE, routes, SEARCH_DELAY } from '../../app/constants/constants'
 import { useAppDispatch, useAppSelector } from '../../app/hooks/hooks'
 import { useDebounce } from '../../app/hooks/useDebounce'
+import { decodeQueryParams, encodeQueryParams } from '../../app/utils/utils'
 import type { IEquipmentItem, ISearchArg } from '../../models/equipments'
 import { useAddTermToHistoryMutation } from '../../store/api/equipment/equipments-api'
-import { clearEquipmentSearch, setSearchFilters, setSearchQueryParams, setSearchTerm } from '../../store/equipments-slice'
+import {
+  clearEquipmentSearch,
+  setSearchFilters,
+  setSearchQueryParams,
+  setSearchTerm,
+} from '../../store/equipments-slice'
 import {
   selectEquipmentSearchFilters,
   selectEquipmentSearchQueryParams,
@@ -21,8 +29,6 @@ import {
   selectLogin,
   selectState,
 } from '../../store/selectors'
-import zIndex from '@mui/material/styles/zIndex'
-import { decodeQueryParams, encodeQueryParams } from '../../app/utils/utils'
 
 interface ISearch {
   list?: IEquipmentItem[] | undefined
@@ -59,41 +65,55 @@ export function Search(props: ISearch) {
       ...(inputValue && { term: inputValue }),
     }
     const encodedParams = encodeQueryParams(params)
-    history.replaceState({}, '', encodedParams)  
+    // eslint-disable-next-line no-restricted-globals
+    history.replaceState({}, '', encodedParams)
   }
 
   // Первый рендер. Который выполнится только на странице Search
   useLayoutEffect(() => {
     // Проверяем, если компонент вмонтирован не на странице Search прекращаем выполнение
-    if(!fetchEquipments || path !== routes.search) return
+    if (!fetchEquipments || path !== routes.search) {
+      return
+    }
     // Проверяем вставил ли пользователь ссылку в поле поиска или же он попал на страницу Search с главной страницы
-    if(initialRender.current) {
+    if (initialRender.current) {
       initialRender.current = false
       const searchParams = location.search
       const { term, filters: initialFilters } = decodeQueryParams(searchParams)
-      
+
       if (term) {
         dispatch(setSearchTerm(term))
       }
-  
+
       if (initialFilters != null) {
         dispatch(setSearchFilters(initialFilters))
       }
+      // secondlRequest.current = false
+      fetchEquipments({
+        login,
+        ...(initialFilters && { filters: initialFilters }),
+        searchTerm: term,
+        page: PAGE,
+        pageSize: PAGE_SIZE,
+      })
     }
-    
   }, [])
 
   useLayoutEffect(() => {
-    if(!fetchEquipments || path !== routes.search) return
-    if(!inputValue && !filters) return
+    if (!fetchEquipments || path !== routes.search) {
+      return
+    }
+    if (!inputValue && !filters) {
+      return
+    }
     fetchEquipments({
       login,
-      filters,
+      ...(filters && { filters }),
       searchTerm: inputValue,
       page: PAGE,
-      pageSize: PAGE_SIZE
+      pageSize: PAGE_SIZE,
     })
-}, [])
+  }, [])
 
   useEffect(() => {
     const handleBackButton = (event: PopStateEvent) => {
@@ -103,39 +123,43 @@ export function Search(props: ISearch) {
     return () => window.removeEventListener('popstate', handleBackButton)
   }, [])
 
-    // Обработка запросов
-    useEffect(() => {
-      if (!fetchEquipments || path === routes.main) return
-      if(!inputValue && !filters) return
-      const abortController = new AbortController();
-      if(initialRequest.current) {
-        initialRequest.current = false
-        return
-      }
-      if(secondlRequest.current) {
-        secondlRequest.current = false
-        return
-      }
-      fetchEquipments({
-        login,
-        filters,
-        searchTerm: inputValue,
-        page: PAGE,
-        pageSize: PAGE_SIZE
-      })
-      return () => {
-        abortController.abort(); // Отмена запроса при размонтировании
-      };
-    }, [debouncedValue, filters])
+  // Обработка запросов
+  useEffect(() => {
+    if (!fetchEquipments || path === routes.main) {
+      return
+    }
+    if (!inputValue && !filters) {
+      return
+    }
+    const abortController = new AbortController()
+    if (initialRequest.current) {
+      initialRequest.current = false
+      return
+    }
+    if (secondlRequest.current) {
+      secondlRequest.current = false
+      return
+    }
+    fetchEquipments({
+      login,
+      ...(filters && { filters }),
+      searchTerm: inputValue,
+      page: PAGE,
+      pageSize: PAGE_SIZE,
+    })
+    return () => {
+      abortController.abort() // Отмена запроса при размонтировании
+    }
+  }, [debouncedValue, filters])
 
   // Обновление URL
   useLayoutEffect(() => {
-    if((debouncedValue || filters) && location.pathname === routes.main) {
+    if ((debouncedValue || filters) && location.pathname === routes.main) {
       navigate(routes.search)
       replaceUrl()
       return
     }
-    if((!filters && !inputValue) && location.pathname === routes.search) {
+    if (!filters && !inputValue && location.pathname === routes.search) {
       history.replaceState(null, '', location.pathname)
       return
     } else {
@@ -155,25 +179,34 @@ export function Search(props: ISearch) {
   const showFilterButton = isMobile && isAnyFilterOpen
 
   return (
-    <Stack spacing={2} direction="column" sx={{ marginTop: { xs: "10px", md: "20px" } }}>
+    <Stack spacing={2} direction="column" sx={{ marginTop: { xs: '10px', md: '20px' } }}>
       <Stack spacing={2} direction="row" sx={{ justifyContent: 'center' }}>
         <SearchInput
           handleInputChange={handleInputChange}
-          handleKeyDown={(e) => e.key === 'Enter' && fetchEquipments?.({ login, filters, searchTerm: inputValue, page: PAGE, pageSize: PAGE_SIZE })}
+          handleKeyDown={e =>
+            e.key === 'Enter' &&
+            fetchEquipments?.({
+              login,
+              filters,
+              searchTerm: inputValue,
+              page: PAGE,
+              pageSize: PAGE_SIZE,
+            })
+          }
           list={list}
           isLoading={isLoading}
           inputValue={inputValue}
           value={null}
         />
-        
+
         {showFilterButton && (
           <Button
             variant="contained"
-            sx={{ 
-              height: '35px', 
-              position: "fixed", 
-              bottom: "10vh", 
-              zIndex: zIndex.modal + 1 
+            sx={{
+              height: '35px',
+              position: 'fixed',
+              bottom: '10vh',
+              zIndex: zIndex.modal + 1,
             }}
             onClick={() => setFiltersOpen(false)}
           >
